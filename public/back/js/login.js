cd117c420511149877a8d7567f5375c0b9d3dce2
DragonSky-CN <495764801@qq.com>
2018-11-26 16:02:19 +0800

    登录页登录功能用户体验完善

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -29,6 +29,10 @@ $(function() {
                         min: 2,
                         max: 6,
                         message: '用户名长度必须是 2-6 位！'
+                    },
+                    // 专门用于 ajax 回调提示的说明
+                    callback: {
+                        message: '用户名不存在！'
                     }
                 }
             },
@@ -46,6 +50,10 @@ $(function() {
                         min: 6,
                         max: 12,
                         message: '密码长度必须是 6-12 位！'
+                    },
+                    // 专门用于 ajax 回调提示的说明
+                    callback: {
+                        message: '密码错误！'
                     }
                 }
             }
@@ -75,11 +83,16 @@ $(function() {
                 }
                 if( info.error === 1000 ) {
                     // 用户名不存在
-                    alert(info.message);
+                    // alert(info.message);
+                    // 参数1：字段名称
+                    // 参数2：校验状态
+                    // 参数3：配置规则，用于提示
+                    $('#form').data('bootstrapValidator').updateStatus('username', 'INVALID', 'callback');
                 }
                 if( info.error === 1001 ) {
                     // 密码错误
-                    alert(info.message);
+                    // alert(info.message);
+                    $('#form').data('bootstrapValidator').updateStatus('password', 'INVALID', 'callback');
                 }
             }
         })
